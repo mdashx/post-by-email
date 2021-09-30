e31ac72baf2ed33452b5f45a20fbfdb1aae8bce5
tom <tom@mdashx.com>
2021-09-30 14:32:47 -0600

    Shorten skipif decorator messages

--- tests/test_dev.py
+++ tests/test_dev.py
@@ -9,41 +9,30 @@ from post_by_email.version_commands import deploy, pull_changes, push_changes
 from post_by_email.create_post import save_doc
 
 
-@pytest.mark.skipif(
-    True,
-    reason="Just for use during development, depends on live connection to server.",
-)
+@pytest.mark.skipif(True, reason="for dev")
 def test_get_emails():
     emails = get_emails()
     for email in emails:
         print(email)
 
 
-@pytest.mark.skipif(
-    True,
-    reason="Just for use during development, depends on live connection to server.",
-)
+@pytest.mark.skipif(True, reason="for dev")
 def test_pull_changes():
     pull_changes()
 
 
-@pytest.mark.skipif(
-    True,
-    reason="Just for use during development, depends on live connection to server.",
-)
+@pytest.mark.skipif(True, reason="for dev")
 def test_push_changes():
     push_changes()
 
 
-# @pytest.mark.skipif(
-#     True,
-#     reason="Just for use during development, writes to the filesystem.",
-# )
+@pytest.mark.skipif(True, reason="for dev")
 def test_save_doc():
     slug = "just-a-test"
     doc = "I am a test document"
     save_doc(slug, doc)
 
 
+@pytest.mark.skipif(True, reason="for dev")
 def test_deploy():
     deploy()
